7b9aefdd5f629a6bb1ccad1ff11372a95a41d3a9
brad <bradhaas@rescomp-14-273272.stanford.edu>
2016-04-20 14:59:43 -0700

    modified regex

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /[Dd]oxbot|[Dd]ox]|[Tt]ony/;
+      botRegex = /[Dd]oxbot|[Dd]ox]|[Tt]ony|[Aa]nthony/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
